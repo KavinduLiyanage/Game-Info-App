5c009c0617aa71604a4bd82d5fe73ece513baf09
KavinduLiyanage <sandeepa8956@gmail.com>
2020-06-13 11:25:21 +0530

    implemented backend part1

---
+++ backend/server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const app = express();
+
+const PORT = 4000;
+const cors = require('cors');
+
+const mongoose = require('mongoose');
+const DB = require('./database/DB.js');
+
+app.use(cors());
+app.use(express.json());
+
+//MongoDB Connection
+mongoose.connect(DB.url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+}).then(
+    ()=> {
+        console.log('Database is connected')
+    },
+    err => {
+        console.log('cannot connect to the database' + err)
+    }
+);
+
+
+// Express Route
+const gamesRouter = require('./routes/game.route');
+
+app.use('/games', gamesRouter);
+
+//PORT
+app.listen(PORT,function () {
+    console.log('Server is running on port : ',PORT);
+});
